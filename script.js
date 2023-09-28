e0bc7847135a1b8874b506d4695ab414269178a2
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-28 16:12:42 +0200

    inserimento vue

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-/*const images = [
-    {
-      image: "01.webp",
-      title: "Marvel's Spiderman Miles Morale",
-      text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-    },
-    {
-      image: "02.webp",
-      title: "Ratchet & Clank: Rift Apart",
-      text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-    },
-    {
-      image: "03.webp",
-      title: "Fortnite",
-      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    },
-    {
-      image: "04.webp",
-      title: "Stray",
-      text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-    },
-    {
-      image: "05.webp",
-      title: "Marvel's Avengers",
-      text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with immagini-player and co-operative gameplay.",
-    },
-  ];
-  
+const{createApp} = Vue;
+createApp ({
+    data(){
+        return{
+            immagine: ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'],
+
+            titolo: ["Marvel's Spiderman Miles Morale", "Ratchet & Clank: Rift Apart", "Fortnite", "Stray", "Marvel's Avengers"],
+
+            testo: ["Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.", "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.", "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city", "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with immagini-player and co-operative gameplay."]
+        }
+    }
+
+}).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+      
+   /*
   const principale = document.getElementById("principale");
   
   const secondarie = document.getElementById("secondarie");
